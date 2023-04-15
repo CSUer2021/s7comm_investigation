4e828065f564a7e936d0b655835686c93ca77c9a
CSUer2021 <1048057242@qq.com>
2023-04-15 22:32:57 +0800

    增加s7comm_10数据集

--- client.py
+++ client.py
@@ -9,17 +9,17 @@ client = Client()
 client.connect('127.0.0.1', 0, 2, SERVER_PORT)
 
 try:
-    i = 0
-    while i < 1000:
-        print(client.get_plc_datetime())
-        client.get_cpu_info()
-        print(client.list_blocks())
-        print(str(client.db_read(1, 0, 100)))
-        client.db_write(1, 0, bytearray(b"HELLO, SERVER!"))
-        print(str(client.db_read(1, 0, 100)))
-        # client.disconnect()
-        print(client.get_connected())
-        i += 1
-        client.plc_stop()
+    # i = 0
+    # while i < 1000:
+    print(client.get_plc_datetime())
+    client.get_cpu_info()
+    print(client.list_blocks())
+    print(str(client.db_read(1, 0, 100)))
+    client.db_write(1, 0, bytearray(b"HELLO, SERVER!"))
+    print(str(client.db_read(1, 0, 100)))
+    client.disconnect()
+    print(client.get_connected())
+    # i += 1
+    client.plc_stop()
 except RuntimeError as ex:
     print(ex)
